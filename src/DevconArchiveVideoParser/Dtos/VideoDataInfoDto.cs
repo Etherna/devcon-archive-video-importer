f9e397931f6459fe788e3d9ae50df8984b311b27
Federico Cicciarella <federico.cicciarella@gmail.com>
2022-10-09 01:41:16 +0200

    readd field IpfsHash

--- src/DevconArchiveVideoParser/Dtos/VideoDataInfoDto.cs
+++ src/DevconArchiveVideoParser/Dtos/VideoDataInfoDto.cs
@@ -7,6 +7,7 @@ namespace DevconArchiveVideoParser.Dtos
         public string? Description { get; set; }
         public int Duration { get; set; }
         public int Edition { get; set; }
+        public string? IpfsHash { get; set; }
         public string? Title { get; set; }
         public string? Type { get; set; }
         public string? YoutubeUrl { get; set; }
