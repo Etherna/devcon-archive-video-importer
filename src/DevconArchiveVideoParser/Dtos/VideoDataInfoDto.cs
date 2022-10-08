ac47d190a3c3be5d7e192e445dca791fef7927b5
Federico Cicciarella <federico.cicciarella@gmail.com>
2022-10-09 01:19:40 +0200

    remove extra data

--- src/DevconArchiveVideoParser/Dtos/VideoDataInfoDto.cs
+++ src/DevconArchiveVideoParser/Dtos/VideoDataInfoDto.cs
@@ -7,13 +7,8 @@ namespace DevconArchiveVideoParser.Dtos
         public string? Description { get; set; }
         public int Duration { get; set; }
         public int Edition { get; set; }
-        public string? Expertise { get; set; }
-        public string? IpfsHash { get; set; }
-        public IEnumerable<string>? Keywords { get; set; }
-        public IEnumerable<string>? Tags { get; set; }
         public string? Title { get; set; }
         public string? Type { get; set; }
-        public string? Track { get; set; }
         public string? YoutubeUrl { get; set; }
     }
 }
